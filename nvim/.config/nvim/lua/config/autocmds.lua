07179d0f9336c402faf3fb60a0a431823020f08a
TomasDanielExpositoTorre <expositotomas2002@gmail.com>
2025-07-02 23:23:21 +0200

    markdown render config

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,20 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Remove spell suggestions
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+  end,
+})
+
+-- Disable markdown rendering
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.cmd("silent! RenderMarkdown disable")
+  end,
+})
