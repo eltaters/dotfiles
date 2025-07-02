ade34d76fdd69df8c942318bcb7b4f74690579a0
TomasDanielExpositoTorre <expositotomas2002@gmail.com>
2025-07-02 14:58:33 +0200

    nvim config for rust + removed arrow movement

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Remove arrow keys from navigation in normal mode
+vim.keymap.set("n", "<Up>", "")
+vim.keymap.set("n", "<Down>", "")
+vim.keymap.set("n", "<Left>", "")
+vim.keymap.set("n", "<Right>", "")
+
+-- Do the same for insert mode
+vim.keymap.set("i", "<Up>", "")
+vim.keymap.set("i", "<Down>", "")
+vim.keymap.set("i", "<Left>", "")
+vim.keymap.set("i", "<Right>", "")
