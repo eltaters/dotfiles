7fae5e82a271f431038f271c74e943b63eb73c19
TomasDanielExpositoTorre <expositotomas2002@gmail.com>
2025-07-02 22:05:18 +0200

    completion disable toggle and arrow re-enable for insert mode

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,9 +7,3 @@ vim.keymap.set("n", "<Up>", "")
 vim.keymap.set("n", "<Down>", "")
 vim.keymap.set("n", "<Left>", "")
 vim.keymap.set("n", "<Right>", "")
-
--- Do the same for insert mode
-vim.keymap.set("i", "<Up>", "")
-vim.keymap.set("i", "<Down>", "")
-vim.keymap.set("i", "<Left>", "")
-vim.keymap.set("i", "<Right>", "")
