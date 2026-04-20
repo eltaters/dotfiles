75042032618b8da2414a7077e844ebb18507bc7a
eltaters <expositotomas2002@gmail.com>
2026-04-20 22:49:59 +0200

    added nvim surround and changed aspect of lualine

--- nvim/.config/nvim/lua/plugins/ui/lualine.lua
+++ nvim/.config/nvim/lua/plugins/ui/lualine.lua
@@ -6,6 +6,17 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
-    theme = "nord",
-  },
+    options = {
+      component_separators = { left = '', right = '' },
+      section_separators   = { left = '', right = '' },
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = { 'filename' },
+      lualine_x = { 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' }
+    },
+  }
 }
